1d4fb9ae521e1689df74284e64b560fbcd6c0d12
Tommy D. Rossi <beats.by.morse@gmail.com>
2026-01-11 19:21:04 +0100

    nicer google

--- tool/googlesearch.ts
+++ tool/googlesearch.ts
@@ -12,7 +12,13 @@ const google = createGoogleGenerativeAI({
 
 const search = tool({
   description:
-    "Search for a query on the internet using Google. This tool can be used in parallel with many independent description parameters",
+    `
+    For the query: A detailed description of what should be searched for. use a long descriptive phrase that describes what you are searching for. Using good english and comprehensive. Tell what is the goal of the query, what this search query should accomplish and what data should be gathered.
+
+    Search for a query on the internet using Google. This tool can be used in parallel with many independent description parameters
+
+    Reading .d.ts files is preferable over google if possible. Only do google searches if the local .d.ts are not enough. Or you cannot directly download relevant source code repo into ./tmp and start a task to explore and answer questions
+    `,
   args: {
     query: tool.schema
       .string()
@@ -23,8 +29,7 @@ const search = tool({
 
   async execute(args, { abort }) {
     const { text } = await generateText({
-      model: google("gemini-2.5-flash"),
-
+      model: google("gemini-2.5-flash-lite"),
       providerOptions: {
         google: {
           // thinkingConfig: {
@@ -39,26 +44,20 @@ const search = tool({
       stopWhen: () => false,
       prompt: `
 
-      For the query: A detailed description of what should be searched for. use a long descriptive phrase that describes what you are searching for. Using good english and comprehensive. Tell what is the goal of the query, what this search query should accomplish and what data should be gathered.
-
-
       Search the web for the following in-depth query: ${JSON.stringify(args.query)}.
 
       This search query was submitted by a coding agent, with the purpose of searching the web to find out how to accomplish something via code or terminal commands.
 
       Summarize in concise bullet points. Include code snippets.
 
-      Do an in deep research of the argument, do not take the first few search results. instead return an in depth summary by reading all necessary articles are resources on the argument. Return code examples if possible.
+      Do an in deep research of the argument, searching multiple times. return an in depth summary by reading all necessary results are resources on the argument. Return code examples if possible.
 
-      The search results will be used by a coding agent, so provide example usage implementation for code if relevant.
+      The search results will be used by a coding agent, so mention docs urls and github repos if present.
 
-      If you find github repository urls in the results, include them in the summary
+      Do not  make up content in your response. Quote directly the content you found and add examples. Do not try to explain the query yourself. Just report all the found sources and quote them if they are relevant.
 
-      Do not try to make up content in your reponse. Quote directly the content you found and add examples. Do not try to explain the query yourself. Just report all the found sources and quote them if they are releant. Find as many sources as possible to answer the question.
+      At the end of the result write "if this search result was not exhaustive enough or did not provide a good enough response update the query and do a new google search. multiple calls in parallel with different strategies to search faster"
 
-      At the end of the result write "if this search result was not exhaustive enought or did not provide a good enough repsponse update the query and do a new websearch. do multiple websearch tool calls in parallel with different strategies to search if possible"
-
-      Reading .d.ts files is preferable over websearch if possible. Only do websearch if the local .d.ts are not enough
       `,
     });
 
