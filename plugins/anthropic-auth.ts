fe7d7fa8d220d3acfda02a7037d55f80976c2ad1
Tommy D. Rossi <beats.by.morse@gmail.com>
2026-03-22 00:51:16 +0100

    Update anthropic-auth.ts

--- plugins/anthropic-auth.ts
+++ plugins/anthropic-auth.ts
@@ -1,3 +1,7 @@
 // moved to https://github.com/remorses/kimaki/blob/main/discord/src/anthropic-auth-plugin.ts
 
-export * from "/Users/morse/Documents/GitHub/kimakivoice/discord/src/anthropic-auth-plugin.ts";
+import * as plugin from "/Users/morse/Documents/GitHub/kimakivoice/discord/src/anthropic-auth-plugin.ts";
+
+let p = process.env.KIMAKI ? undefined : plugin.anthropicAuthPlugin
+
+export default p
